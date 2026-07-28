ee80b425e64bd7bc2fddd1954ccaea0edd6056c0
Simply Design <staceywhitney@gmail.com>
2026-07-28 13:33:23 -0600

    v1.0.32 — force column block list to stack with !important for WP 7.x

--- build/column/index.asset.php
+++ build/column/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4bc09116088ed7dd1a3a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bd6a927953204614cd75');
