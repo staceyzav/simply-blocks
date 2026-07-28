0e28a8fffb3a8b6ab4651cfe39fdb11812ebf38e
Simply Design <staceywhitney@gmail.com>
2026-07-28 13:28:43 -0600

    v1.0.31 — fix nested columns stacking in WP 7.x editor

--- build/column/index.asset.php
+++ build/column/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '47180412b34a3ebaf08c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4bc09116088ed7dd1a3a');
